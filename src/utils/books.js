169045ef84931b8a737896b61262885096cf006f
Suman D R <106007784+Suman-D-R@users.noreply.github.com>
2023-12-24 09:15:03 +0530

    [suman] Modified: added apis for wishlist cart and updated books, user apis

---
+++ src/utils/books.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const bearer_token = localStorage.getItem("accessTokenBookstore");
+const headers = {
+  Authorization: `Bearer ${bearer_token}`,
+  "Content-Type": "application/json",
+};
+
+export const bookOperations = async (endpoint = "") => {
+  try {
+    const response = await axios.get(
+      `http://localhost:3000/api/v1/books`,
+      {
+        headers: headers,
+      }
+    );
+
+    const bookData = response.data;
+
+    return bookData;
+  } catch (error) {
+    // Handle errors
+    console.error("Error fetching books:", error);
+    throw error;
+  }
+};
