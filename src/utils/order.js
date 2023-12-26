64a1191274c3646064427eac6482bfe35f1713bc
Suman D R <106007784+Suman-D-R@users.noreply.github.com>
2023-12-26 11:51:47 +0530

    [suman] Modified: created slice for wishlist ,order and added api for order

---
+++ src/utils/order.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+const bearer_token = localStorage.getItem("accessTokenBookstore");
+
+
+export const getOrders = async () => {
+  try {
+    const orderData = await axios.get("http://localhost:3000/api/v1/order", {
+      headers: {
+        Authorization: `Bearer ${bearer_token}`,
+        "Content-Type": "application/json",
+      },
+    });
+    return orderData;
+  } catch (error) {
+    throw error;
+  }
+};
+
+
+export const placeOrder = async (data) => {
+  try {
+    const response = await axios.post(
+      `http://localhost:3000/api/v1/order`,{data},
+      {
+        headers: {
+          Authorization: `Bearer ${bearer_token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const bookData = response.data;
+
+    return bookData;
+  } catch (error) {
+    console.error("Error fetching books:", error);
+    throw error;
+  }
+};
