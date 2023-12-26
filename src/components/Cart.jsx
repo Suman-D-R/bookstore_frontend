4c672d9dafb2fef4d94d7c10984627dbc3a9ce45
Suman D R <106007784+Suman-D-R@users.noreply.github.com>
2023-12-26 11:47:51 +0530

    [suman] Modified: cart component added

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import "../sass/Cart.scss";
 import CartBookCard from "./CartBookCard";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 import Radio from "@mui/material/Radio";
@@ -10,14 +10,74 @@ import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { setOrders } from "../utils/store/orderSlice";
+import { placeOrder } from "../utils/order";
 
 function Cart() {
+  const [customerDetails, setCustomerDetails] = useState(false);
+  const [address, setAddress] = useState(false);
+  const [addressDetails, setAddressDetails] = useState({
+    fullName: "",
+    mobileNumber: "",
+    address: "",
+    city: "",
+    state: "",
+    type: "Home", 
+  });
+
+  const dispatch = useDispatch();
+
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  const navigate = useNavigate();
   const cartData = useSelector((store) => store.cart.cartItems);
   const location = [
-    { label: "Bengloure" },
+    { label: "Bengaluru" },
     { label: "Mysore" },
     { label: "Mumbai" },
   ];
+
+  const handleOrder = () => {
+    setCustomerDetails(true);
+  };
+
+  const handleAddress = () => {
+    setAddress(true);
+  };
+
+  const handleCheckOut = () => {
+    const  data = {...cartData, address :addressDetails}
+    
+    dispatch(setOrders(addressDetails));
+    placeOrder(data);
+
+    
+ 
+    navigate("/home/ordersuccessful");
+  };
+
+  const handleAddressInputChange = (field, value) => {
+    setAddressDetails((prevDetails) => ({
+      ...prevDetails,
+      [field]: value,
+    }));
+  };
+
+  const validateForm = () => {
+    const requiredFields = ["fullName", "mobileNumber", "address", "city", "state"];
+
+    const isValid = requiredFields.every(
+      (field) => addressDetails[field] && addressDetails[field].trim() !== ""
+    );
+
+    setIsFormValid(isValid);
+  };
+
+  useEffect(() => {
+    validateForm();
+  }, [addressDetails]);
 
   return (
     <div className="cart-container">
@@ -40,11 +100,7 @@ function Cart() {
                   options={location}
                   sx={{ width: 200 }}
                   renderInput={(params) => (
-                    <TextField
-                      size="small"
-                      {...params}
-                      placeholder="Location"
-                    />
+                    <TextField size="small" {...params} placeholder="Location" />
                   )}
                 />
               </div>
@@ -55,81 +111,160 @@ function Cart() {
                   <CartBookCard key={val.book_id} cartData={val} />
                 ))}
             </div>
+            {customerDetails ? (
+              <div></div>
+            ) : (
+              <div className="item-content-3">
+                <Button onClick={handleOrder} variant="contained">
+                  Place Order
+                </Button>
+              </div>
+            )}
           </div>
           <div className="cart-content-3">
             <div className="cart-customer-content">
               <h1>Customer Details</h1>
             </div>
-            <div className="cart-address-from">
-              <div className="cart-textcontent">
-                <div className="cart-split-content">
-                  <p>Full Name</p>
-                  <TextField fullWidth id="fullWidth" />
-                </div>
-                <div className="cart-split-content">
-                  <p>Mobile Number</p>
-                  <TextField fullWidth id="fullWidth" />
-                </div>
-              </div>
-              <div>
-                <p>Address</p>
-                <TextField fullWidth id="fullWidth" />
-              </div>
-              <div className="cart-textcontent">
-                <div className="cart-split-content">
-                  <p>City / Town</p>
-                  <TextField fullWidth id="fullWidth" />
-                </div>
-                <div className="cart-split-content">
-                  <p>State </p>
-                  <TextField fullWidth id="fullWidth" />
-                </div>
-              </div>
-              <div>
-                <FormControl>
-                  <FormLabel id="demo-row-radio-buttons-group-label">
-                    Type
-                  </FormLabel>
-                  <RadioGroup
-                    row
-                    aria-labelledby="demo-row-radio-buttons-group-label"
-                    name="row-radio-buttons-group"
-                  >
-                    <FormControlLabel
-                      value="Home"
-                      control={<Radio />}
-                      label="Home"
+            {customerDetails && address === false ? (
+              <div className="cart-address-form">
+                <div className="cart-textcontent">
+                  <div className="cart-split-content">
+                    <p>Full Name</p>
+                    <TextField
+                      fullWidth
+                      id="fullWidth"
+                      onChange={(e) =>
+                        handleAddressInputChange("fullName", e.target.value)
+                      }
                     />
-                    <FormControlLabel
-                      value="Work"
-                      control={<Radio />}
-                      label="Work"
+                  </div>
+                  <div className="cart-split-content">
+                    <p>Mobile Number</p>
+                    <TextField
+                      fullWidth
+                      id="fullWidth"
+                      onChange={(e) =>
+                        handleAddressInputChange("mobileNumber", e.target.value)
+                      }
                     />
-                    <FormControlLabel
-                      value="other"
-                      control={<Radio />}
-                      label="Other"
+                  </div>
+                </div>
+                <div>
+                  <p>Address</p>
+                  <TextField
+                    fullWidth
+                    id="fullWidth"
+                    onChange={(e) =>
+                      handleAddressInputChange("address", e.target.value)
+                    }
+                  />
+                </div>
+                <div className="cart-textcontent">
+                  <div className="cart-split-content">
+                    <p>City / Town</p>
+                    <TextField
+                      fullWidth
+                      id="fullWidth"
+                      onChange={(e) =>
+                        handleAddressInputChange("city", e.target.value)
+                      }
                     />
-                  </RadioGroup>
-                </FormControl>
+                  </div>
+                  <div className="cart-split-content">
+                    <p>State </p>
+                    <TextField
+                      fullWidth
+                      id="fullWidth"
+                      onChange={(e) =>
+                        handleAddressInputChange("state", e.target.value)
+                      }
+                    />
+                  </div>
+                </div>
+                <div>
+                  <FormControl>
+                    <FormLabel id="demo-row-radio-buttons-group-label">
+                      Type
+                    </FormLabel>
+                    <RadioGroup
+                      row
+                      aria-labelledby="demo-row-radio-buttons-group-label"
+                      name="row-radio-buttons-group"
+                    >
+                      <FormControlLabel
+                        value="Home"
+                        label="Home"
+                        control={<Radio />}
+                        onChange={(e) =>
+                          handleAddressInputChange("type", e.target.value)
+                        }
+                      />
+                      <FormControlLabel
+                        value="Work"
+                        control={<Radio />}
+                        label="Work"
+                        onChange={(e) =>
+                          handleAddressInputChange("type", e.target.value)
+                        }
+                      />
+                      <FormControlLabel
+                        value="other"
+                        control={<Radio />}
+                        onChange={(e) =>
+                          handleAddressInputChange("type", e.target.value)
+                        }
+                      />
+                    </RadioGroup>
+                  </FormControl>
+                </div>
+                <div>
+                  {address ? (
+                    <div></div>
+                  ) : (
+                    <div className="item-content-3">
+                      <Button
+                        onClick={handleAddress}
+                        variant="contained"
+                        disabled={!isFormValid}
+                      >
+                        Continue
+                      </Button>
+                    </div>
+                  )}
+                </div>
               </div>
-            </div>
+            ) : (
+              <div></div>
+            )}
           </div>
           <div className="cart-content-4">
             <div>
-              <h2>Order summery</h2>
-              <div>
-                <div><img src={cartData.items[1].bookImage} alt="" /></div>
-                <div>
-                  <div>{cartData.items[1].booName}</div>
-                  <p>author</p>
-                  <div>
-                  <h3>Rs. 500</h3>
-                  </div>
-                  
+              <h2>Order summary</h2>
+              {address ? (
+                <div className="order-summery">
+                  {cartData.items &&
+                    cartData.items.map((val, index) => (
+                      <CartBookCard
+                        key={val.book_id}
+                        cartData={val}
+                        summery={true}
+                      />
+                    ))}
                 </div>
+              ) : (
+                <div></div>
+              )}
+              <div>
+                {address ? (
+                  <div className="item-content-3">
+                    <Button onClick={handleCheckOut} variant="contained">
+                      Check Out
+                    </Button>
+                  </div>
+                ) : (
+                  <div></div>
+                )}
               </div>
-              <div></div>
             </div>
           </div>
         </div>
